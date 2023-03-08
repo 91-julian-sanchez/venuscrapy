fcf2d0fcdfdfd3f1675aff7a7beefd6006d30144
91-julian-sanchez <91.julian.sanchez@gmail.com>
2023-03-08 00:25:24 -0500

    feat: change path for read csv to analize data

--- analyze/analysis.py
+++ analyze/analysis.py
@@ -1,11 +1,13 @@
 # El valor de correlación varía entre -1 y 1, donde -1 indica una correlación negativa fuerte, 1 indica una correlación positiva fuerte y 0 indica una falta de correlación.
 
-# Si la correlación es positiva y cercana a 1, significa que a medida que aumenta la edad, también aumentan las impresiones. Si la correlación es negativa y cercana a -1, significa que a medida que aumenta la edad, disminuyen las impresiones. Si la correlación es cercana a 0, significa que no hay una relación clara entre las dos variables.
+# Si la correlación es positiva y cercana a 1, significa que a medida que aumenta la edad, también aumentan las impresiones. 
+# Si la correlación es negativa y cercana a -1, significa que a medida que aumenta la edad, disminuyen las impresiones. 
+# Si la correlación es cercana a 0, significa que no hay una relación clara entre las dos variables.
 
 import pandas as pd
 
 # Cargue su dataframe
-df = pd.read_csv("output/chaturbate-latina-sort.csv")
+df = pd.read_csv("../output/load/latina.csv")
 
 # Calcular la correlación entre las dos columnas
 correlation = df['age'].corr(df['impression'])
